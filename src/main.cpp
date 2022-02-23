10888cb2a2e24aab9c82b5c9eef311dd685abd7d
Daniel Herzog <wikiti.doghound@gmail.com>
2022-02-23 13:39:41 +0000

    Integrating x^2

--- src/main.cpp
+++ src/main.cpp
@@ -6,36 +6,16 @@ using namespace BRT;
 
 int main()
 {
-  int inside_circle = 0;
-  int inside_circle_stratified = 0;
-  int sqrt_N = 10000;
-  auto N = static_cast<double>(sqrt_N) * sqrt_N;
-
-  for (int i = 0; i < sqrt_N; ++i)
+  int N = 1000000;
+  auto sum = 0.0;
+  for (int i = 0; i < N; i++)
   {
-    for (int j = 0; j < sqrt_N; ++j)
-    {
-      // General
-      auto x = Utils::Random(-1.0, 1.0);
-      auto y = Utils::Random(-1.0, 1.0);
-      if (x*x + y*y < 1)
-      {
-        ++inside_circle;
-      }
-
-      // Stratified
-      x = Utils::Remap(((i + Utils::Random()) / sqrt_N), 0, 1, -1, 1);
-      y = Utils::Remap(((j + Utils::Random()) / sqrt_N), 0, 1, -1, 1);
-      if (x*x + y*y < 1)
-      {
-        ++inside_circle_stratified;
-      }
-    }
+    auto x = Utils::Random(0.0, 2.0);
+    sum += x * x;
   }
-
   std::cout << std::fixed << std::setprecision(12);
-  std::cout << "Regular    Estimate of Pi = " << 4 * double(inside_circle) / N << '\n';
-  std::cout << "Stratified Estimate of Pi = " << 4 * double(inside_circle_stratified) / N << '\n';
+  std::cout << "I   = " << 2 * sum / N << '\n';
+  std::cout << "8/3 = " << 8.0 / 3 << '\n';
 
   return 0;
 }
