80a7e52754ebae6c33e862acb7b7db847773b4ed
Daniel Herzog <wikiti.doghound@gmail.com>
2022-02-23 13:54:30 +0000

    Constructing a PDF

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,22 @@
 
 using namespace BRT;
 
+inline double pdf(double x)
+{
+  return 0.5 * x;
+}
+
 int main()
 {
   int N = 1000000;
   auto sum = 0.0;
   for (int i = 0; i < N; i++)
   {
-    auto x = Utils::Random(0.0, 2.0);
-    sum += x * x;
+    auto x = std::sqrt(Utils::Random(0.0, 4.0));
+    sum += x * x / pdf(x);
   }
   std::cout << std::fixed << std::setprecision(12);
-  std::cout << "I   = " << 2 * sum / N << '\n';
+  std::cout << "I   = " << sum / N << '\n';
   std::cout << "8/3 = " << 8.0 / 3 << '\n';
 
   return 0;
